739096e4df14bd6e069d188260179a9dd95b6d08
Berino <r9.berino@gmail.com>
2024-03-17 21:13:54 -0300

    c5 - app js

---
+++ book/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import logo from './imagens/logo.svg'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-Header">
+      <div className="logo">
+      <img src={logo}></img>
+      <p>Teste</p>
+      </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
